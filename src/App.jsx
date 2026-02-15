297e510f7519e2f01297fd83a9dc0345950a65b8
Al ameen Purkait <alameenpurkait244@gmail.com>
2026-02-15 20:50:07 +0530

    deleting testing.jsx file(unused)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import MainLayout from "./Layouts/MainLayout";
+import AuthLayout from "./Layouts/AuthLayout";
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<AuthLayout />}></Route>
         </Routes>
       </Router>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
