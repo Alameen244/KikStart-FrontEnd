69cfc438f5e5b7efb5e8479d10c84690c23a8892
parnasree1 <parnasreepaul12@gmail.com>
2026-03-07 22:37:48 +0530

    form

--- src/Components/FormComponents/FormComponent.jsx
+++ src/Components/FormComponents/FormComponent.jsx
@@ -11,12 +11,29 @@ const LayerWrapper = styled("div")({});
 const LayerContainer = styled(Container)({});
 
 const ContentWrapper = styled(Box)({
-  marginTop: "32px",
   justifyContent: "center",
   display: "flex",
   border: "1px solid #E5E5E5",
   borderRadius: "30px",
-  padding: "20px",
+  padding: "0px 20px",
+});
+
+const Headingwrapper = styled("div")({
+  textAlign: "center",
+  paddingTop: "80px",
+  paddingBottom: "50px",
+  h2: {
+    fontFamily: "PT Sans",
+    fontSize: "26px",
+    fontWeight: "700",
+    color: "#2B2B2B",
+  },
+  p: {
+    fontFamily: "Noto Sans",
+    fontSize: "15px",
+    fontWeight: "400",
+    color: "#494949",
+  },
 });
 
 const ContentGrid = styled(Grid)({});
@@ -25,6 +42,10 @@ export default function FormComponent() {
   return (
     <LayerWrapper>
       <LayerContainer>
+        <Headingwrapper>
+          <h2>Fill the form</h2>
+          <p>Lorem ipsum dolor sit amet consectetur</p>
+        </Headingwrapper>
         <ContentWrapper>
           <ContentGrid container spacing={4}>
             <Grid size={{ lg: 4 }}>
